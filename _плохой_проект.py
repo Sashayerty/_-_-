17b96c0e412d0d7467e7177e5c7b7fb256170887
Alex <Sashayerty@ya.ru>
2025-04-01 14:26:23 +0300

    Перевел все на патриотичный лад

--- _плохой_проект.py
+++ _плохой_проект.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask as Управлялка
+from langchain.chat_models import init_chat_model as сделай_мне_разговорник
+from pydantic import BaseModel as БазоваяМодель
+from pydantic import Field as Поле
 
-проект = Flask(__name__)
+строка = str
+__имя__ = "__главный__"
+проект = Управлялка(__имя__)
 рука = проект.route
 запускай = проект.run
-__имя__ = "__главный__"
+наша_модель_для_общения = сделай_мне_разговорник(
+    model="mistral-small-latest", model_provider="mistralai"
+)
+
+
+class ВыходныеДанныеОтИИ(БазоваяМодель):
+    идея_проекта: строка = Поле(description="Ты мне должен дать идею проекта")
+    описание_проекта: строка = Поле(description="Описание проекта")
+    убийственная_фишка_проекта: строка = Поле(
+        description="Киллерфича проекта, в чем его прикол."
+    )
 
 
 @рука("/главная", methods=["POST", "GET"])
