43d1ce52533aac002380713d085d724d4f91377c
Alex <Sashayerty@ya.ru>
2025-04-01 14:15:07 +0300

    Перевел все на патриотичный лад

--- _плохой_проект.py
+++ _плохой_проект.py
@@ -1,23 +1,25 @@
 from flask import Flask
 
 проект = Flask(__name__)
+рука = проект.route
+запускай = проект.run
 __имя__ = "__главный__"
 
 
-@проект.route("/", methods=["POST", "GET"])
+@рука("/главная", methods=["POST", "GET"])
 def поехали():
     return "Привет"
 
 
-@проект.route("/зарегистрируй-пж", methods=["POST", "GET"])
+@рука("/зарегистрируй-пж", methods=["POST", "GET"])
 def зарегистрируй_пж():
     return "ок"
 
 
-@проект.route("/войди-пж", methods=["POST", "GET"])
+@рука("/войди-пж", methods=["POST", "GET"])
 def войди_пж():
     return "да"
 
 
 if __имя__ == "__главный__":
-    проект.run(debug="Правда")
+    запускай(debug="Правда")
